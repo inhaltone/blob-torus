da1acdebf2767b311fcc06287f48c00ba9dd82cc
inhaltone <inhalt.xyz@gmail.com>
2023-04-11 14:13:53 +0300

    add: testing geometry

--- src/blobject.js
+++ src/blobject.js
@@ -8,6 +8,8 @@ import {GammaCorrectionShader} from "three/addons/shaders/GammaCorrectionShader.
 import {BleachBypassShader} from "three/addons/shaders/BleachBypassShader.js";
 import {FilmPass} from "three/addons/postprocessing/FilmPass.js";
 import {noise} from "./noise.js";
+import {ColorCorrectionShader} from "three/addons/shaders/ColorCorrectionShader.js";
+import {HueSaturationShader} from "three/addons/shaders/HueSaturationShader.js";
 
 const BG_COLOR = new THREE.Color(0.6, 0.6, 0.6, 0);
 const COLOR_A = new THREE.Color(231, 50, 8);
@@ -28,7 +30,9 @@ export default function renderBlob(props) {
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 3.5;
 
-    const geometry = new THREE.TorusGeometry(16, 6, 32, 100);
+    // const geometry = new THREE.TorusGeometry(16, 6, 32, 100);
+    // const geometry = new THREE.TetrahedronGeometry( 5, 24 );
+    const geometry = new THREE.TorusGeometry(64, 24, 32, 100);
     const material = new THREE.MeshPhongMaterial({
         color: 0xf13804,
         specular: 0xffffff,
@@ -36,12 +40,12 @@ export default function renderBlob(props) {
         reflectivity: 1.,
         // emissive: 0xf13804
     });
-    const sphere = new THREE.Mesh(geometry, material);
+    const sphere = new THREE.Mesh(geometry, buildTwistMaterial(1.2));
     scene.add(sphere);
 
     // Light the scene
-    const godLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
-    godLight.color.setHSL(0.1, 1, 0.1);
+    const godLight = new THREE.HemisphereLight(0xf13804, 0xffffff, 1.2);
+    godLight.color.setHSL(.1, 1, 0.1);
     godLight.groundColor.setHSL(0.095, 1, 0.75);
     godLight.position.set(0, 50, 0);
     scene.add(godLight);
@@ -69,13 +73,17 @@ export default function renderBlob(props) {
 
     // go for grain
     const gammaCorrection = new ShaderPass(GammaCorrectionShader);
+    const colorCorrection = new ShaderPass(ColorCorrectionShader);
+    const hueSaturationShader = new ShaderPass(HueSaturationShader);
     const effectBleach = new ShaderPass(BleachBypassShader);
     effectBleach.uniforms['opacity'].value = 0.5;
-    const effectFilm = new FilmPass(.8, 0, 0, false);
-    // composer.addPass(gammaCorrection);
-
+    const effectFilm = new FilmPass(.6, 0, 0, false);
+    hueSaturationShader.uniforms['hue'].value = -0.1;
+    hueSaturationShader.uniforms['saturation'].value = .1;
     composer.addPass(effectBleach);
     composer.addPass(effectFilm);
+    composer.addPass(hueSaturationShader);
+
 
     const update = () => {
 
@@ -86,8 +94,8 @@ export default function renderBlob(props) {
             const v3 = new THREE.Vector3();
             v3.fromBufferAttribute(positions, i).setLength(k);
             // v3.normalize().multiplyScalar(1 + 0.4 * noise.perlin3(v3.x * k + time, v3.y * k, v3.z * k));
-            let n = noise.perlin3(v3.x * k + time, v3.y * k + time, v3.z);
-            v3.setLength(1 + 0.3 * n);
+            let n = noise.perlin3(v3.x * k + time, v3.y * k + time, v3.z * 2 + time);
+            v3.setLength(1 + 0.3 * n); // TODO: sizing here need some improvement
             positions.setXYZ(i, v3.x, v3.y, v3.z);
         }
         // positions.needsUpdate = true;
@@ -112,20 +120,86 @@ export default function renderBlob(props) {
 
     function animate() {
         const time = Date.now() * 0.0005;
-        // sphere.rotation.x += 0.001;
-        // sphere.rotation.x = Math.sin(time * 0.2) * 2;
-        // sphere.rotation.y = Math.sin(time * 0.8) * 2;
-        sphere.rotation.z += 0.004;
 
-        directionalLight.position.x = Math.sin(time * 0.7) * 1.2;
-        directionalLight.position.y = Math.cos(time * 0.5) * 2;
-        directionalLight.position.z = Math.cos(time * 0.3) * 2;
+        sphere.rotation.z = Math.sin(time * 0.3) * 2;
+        sphere.rotation.x = Math.cos(time * 0.1) * 3.33;
+
+        directionalLight.position.x = Math.sin(time * 0.8) * 100;
+        directionalLight.position.y = Math.cos(time * 0.5) * 50;
+        directionalLight.position.z = Math.cos(time * 0.8) * 100;
+
+
+        hueSaturationShader.uniforms['hue'].value = Math.cos(time * 0.5) * 0.4;
 
         update();
         /* render scene and camera */
-        renderer.render(scene, camera);
+        render();
         composer.render();
         requestAnimationFrame(animate);
+    }
+
+    function render() {
+
+        scene.traverse(function (child) {
+
+            if (child.isMesh) {
+
+                const shader = child.material.userData.shader;
+
+                if (shader) {
+
+                    shader.uniforms.time.value = performance.now() / 8000;
+
+                }
+
+            }
+
+        });
+
+        renderer.render(scene, camera);
+
+    }
+
+    function buildTwistMaterial(amount) {
+
+        const material = new THREE.MeshPhongMaterial({
+            color: 0xf13804,
+            specular: 0xffffff,
+            shininess: 88.8,
+            reflectivity: 1.,
+            // emissive: 0xf13804
+        });
+        material.onBeforeCompile = function (shader) {
+
+            shader.uniforms.time = {value: 0};
+
+            shader.vertexShader = 'uniform float time;\n' + shader.vertexShader;
+            shader.vertexShader = shader.vertexShader.replace(
+                '#include <begin_vertex>',
+                [
+                    `float theta = sin( time + position.y ) / ${amount.toFixed(1)};`,
+                    'float c = cos( theta );',
+                    'float s = sin( theta );',
+                    'mat3 m = mat3( c, 0, s, 0, 1, 0, -s, 0, c );',
+                    'vec3 transformed = vec3( position ) * m;',
+                    'vNormal = vNormal * m;'
+                ].join('\n')
+            );
+
+            material.userData.shader = shader;
+
+        };
+
+        // Make sure WebGLRenderer doesnt reuse a single program
+
+        material.customProgramCacheKey = function () {
+
+            return amount;
+
+        };
+
+        return material;
+
     }
 
 
